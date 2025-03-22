ca10191e37d3f326000d0efb8229c30952c973fb
ManuelDAssoro <73363119+ManuelDAssoro@users.noreply.github.com>
2025-03-22 12:31:52 -0300

    add playGame function for complete gameplay

--- code.js
+++ code.js
@@ -19,7 +19,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    humanChoice = "none";
+    let humanChoice = "none";
     while (humanChoice != "rock" && humanChoice != "paper" &&humanChoice != "scissors"){
         let input = prompt("Choose Rock, Paper or Scissors!");
         if (input !== null) {
@@ -30,41 +30,70 @@ function getHumanChoice() {
 }
 
 function playRound (humanChoice, computerChoice){
-    console.log (humanChoice);
-    console.log (computerChoice);
     if (humanChoice == computerChoice){
-        console.log ("You both picked " + humanChoice + ", its a draw!");
+        console.log ("You both picked " + humanChoice + ", it's a draw!");
+        return;
     }
     else {
         if (humanChoice == "rock"){
             if (computerChoice == "paper"){
                 computerScore += 1;
-                console.log("Rock vs Paper: You Lose!")                          
+                console.log("Rock vs Paper: You Lose!") 
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;                         
             }
             else{
                 humanScore += 1;
-                console.log("Rock vs Sciss                  ors: You Win!")
+                console.log("Rock vs Scissors: You Win!")
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;
             }
         }
         else if (humanChoice == "paper"){
             if (computerChoice == "scissors"){
                 computerScore += 1;
-                console.log("Paper vs Scissors: You Lose!")                          
+                console.log("Paper vs Scissors: You Lose!")
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;                          
             }
             else{
                 humanScore += 1;
                 console.log("Paper vs Rock: You Win!")
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;
             }
         }
         else {
             if (computerChoice == "rock"){
                 computerScore += 1;
-                console.log("Scissors vs Rock: You Lose!")                          
+                console.log("Scissors vs Rock: You Lose!")
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;                          
             }
             else{
                 humanScore += 1;
                 console.log("Scissors vs Paper: You Win!")
+                console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+                return;
         }
     }
 }
+}
+
+function playGame() {
+    console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+    humanScore = 0;
+    computerScore = 0;
+    console.log("Your score: " + humanScore + " - Computer score: " + computerScore);
+    while (humanScore < 5 && computerScore < 5){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }        
+    if (humanScore == 5){
+        console.log("You win the game!")
+    }
+    else{
+        console.log("The computer wins the game!")
+    }
 }
